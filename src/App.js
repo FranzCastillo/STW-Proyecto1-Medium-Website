8342f3aae469a226cf05301262c6557e2d720861
Francisco Castillo <cas21562@uvg.edu.gt>
2023-05-04 14:54:04 -0600

    NavBar re implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
+
 const App = () => {
-    return(
-      <div>
-        <NavBar />
-      </div>
+    return (
+        <div>
+            <NavBar />
+        </div>
     );
 };
 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,58 +1,18 @@
 import React from "react";
+import Logo from "./Logo";
 import "./NavBar.scss";
-import Logo from "../Logo/Logo";
 
 const NavBar = () => {
     return (
-        <div className={"nav-bar-wrapper"}>
-            <div className={"nav-bar"}>
-                <div className={"content"}>
-                    <div className={"text"}>
-                        <div id={"logo"}>
-                            <a href={"/"}>
-                                <Logo />
-                            </a>
-                        </div>
-                        <div id={"separator"} />
-                        <div className={"links"}>
-                            <span className={"link"}>
-                                <div className={"link-text"}>
-                                    <p>
-                                        <a href={"/"}>Our Story</a>
-                                    </p>
-                                </div>
-                            </span>
-                            <span className={"link"}>
-                                <div className={"link-text"}>
-                                    <p>
-                                        <a href={"/"}>Membership</a>
-                                    </p>
-                                </div>
-                            </span>
-                            <span className={"link"}>
-                                <div className={"link-text"}>
-                                    <p>
-                                        <a href={"/"}>Write</a>
-                                    </p>
-                                </div>
-                            </span>
-                            <span className={"link"}>
-                                <div className={"link-text"}>
-                                    <p>
-                                        <a href={"/"}>Sign In</a>
-                                    </p>
-                                </div>
-                            </span>
-                            <div className={"started-button"}>
-                                <span>
-                                    <a className={"started-button-link"} href={"/"}>
-                                        <button id={"started-button-button"}>Get started</button>
-                                    </a>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div className={"nav-bar"}>
+            <Logo/>
+            <div id={"separator"}></div>
+            <div className={"nav-bar-links"}>
+                <a href={"#"}>Our story</a>
+                <a href={"#"}>Membership</a>
+                <a href={"#"}>Write</a>
+                <a href={"#"}>Sign In</a>
+                <button>Get started</button>
             </div>
         </div>
     );
