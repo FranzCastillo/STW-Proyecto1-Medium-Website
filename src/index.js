ea02b1905780705f56568dd3c374406f39d4de11
Francisco Castillo <cas21562@uvg.edu.gt>
2023-05-06 22:44:36 -0600

    Create-react-app Implemented

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./App.scss";
-
-const el = document.getElementById("root");
-
-ReactDOM.createRoot(el).render(<App />);
