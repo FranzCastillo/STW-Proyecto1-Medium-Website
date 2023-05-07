f50c65363c053ca782e2d224615e5db87e9e9499
Francisco Castillo <cas21562@uvg.edu.gt>
2023-05-07 14:48:42 -0600

    SSR Final Implementation

--- app/pages/api/ssr.js
+++
@@ -1,7 +0,0 @@
-import { renderToString } from 'react-dom/server'
-import Banner from '../../components/Banner/Banner'
-
-export default function handler(req, res) {
-    const html = renderToString(<Banner />)
-    res.status(200).send(html)
-}
