566946aab623ecd34c7ce19de1bead318539d21e
Francisco Castillo <cas21562@uvg.edu.gt>
2023-05-06 18:08:23 -0600

    Nav Bar color change

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,21 +1,40 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Logo from "./Logo";
 import "./NavBar.scss";
 
 const NavBar = () => {
-    return (
-        <div className={"nav-bar"}>
-            <Logo/>
-            <div id={"separator"}></div>
-            <div className={"nav-bar-links"}>
-                <a href={"#"}>Our story</a>
-                <a href={"#"}>Membership</a>
-                <a href={"#"}>Write</a>
-                <a href={"#"}>Sign In</a>
-                <button>Get started</button>
-            </div>
-        </div>
-    );
-}
+    const [isYellow, setIsYellow] = useState(true);
 
-export default NavBar;
+    useEffect(() => {
+        function handleScroll() {
+            const currentScrollY = window.scrollY;
+            if (currentScrollY > 300) {
+                setIsYellow(false);
+            } else {
+                setIsYellow(true);
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+        return (
+            <div className={isYellow? "nav-bar-yellow" : "nav-bar-white"}>
+                <Logo/>
+                <div id={"separator"}></div>
+                <div className={"nav-bar-links"}>
+                    <a href={"#"}>Our story</a>
+                    <a href={"#"}>Membership</a>
+                    <a href={"#"}>Write</a>
+                    <a href={"#"}>Sign In</a>
+                    <button>Get started</button>
+                </div>
+            </div>
+        );
+    }
+
+    export default NavBar;
